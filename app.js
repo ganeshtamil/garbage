8eab2422b81a545f351de2945a4d759b03ecfe6a
Ganeshtamil <70477131+ganeshtamil@users.noreply.github.com>
2022-05-13 12:49:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".btn").click(function(){
+        $(".submenu").slideToggle(200);
+    });
+});
+
+$(function(){
+    $(".navbar i").click(function(){
+        $(".menu").slideToggle(100);
+    });
+});
